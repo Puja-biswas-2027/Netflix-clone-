7324f5382c8734ef8b1192e59e6f502f09f2ddb6
Puja-biswas-2027 <pujabiswas25101992@gmail.com>
2021-08-24 01:41:53 +0600

    first commit

---
+++ Archive.js
@@ -0,0 +1,17 @@
+//tmdb api key
+//caa8c1a2238ac8a1622273b602d30866
+// tmdb request token
+//https://api.themoviedb.org/3/movie/550?api_key=caa8c1a2238ac8a1622273b602d30866
+//npm install -g firebase-tools
+// fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+//fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+//fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+//fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+//fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+//fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+//fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+//fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+
+//netflix logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+// user avater:"https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
